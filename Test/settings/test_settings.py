20a9cacaee761400c60b8116ec7ec36b74c93ddd
KabirovI <ilnar.kabirov.98@mail.ru>
2023-12-21 20:21:28 +0300

    Вариант 7

--- Test/settings/test_settings.py
+++ Test/settings/test_settings.py
@@ -4,15 +4,15 @@ from Test.test_base import TestBase
 class TestSettings(TestBase):
 
     def test_view_schedule_from_me(self):
+        # Вариант 7 Кабиров И.Р. 20ИО
+        #
         # Нажимаем на кнопку Шестеренки
         self.APP.sidebar.click_on_settings()
-        # Меняем режим отображения расписания для пользователя
-        self.APP.tab_settings.click_button_change_view()
         # Выбираем кафедру
-        self.APP.tab_settings.select_cathedra('Гуманитарные науки')
+        self.APP.tab_settings.select_cathedra('Информационные технологии и системы связи')
         # Переходим к расписанию
         self.APP.sidebar.click_on_schedule()
-        # Выбираем преподавателя
-        self.APP.tab_schedule.select_teacher('Бухалова Н.А.')
-        # Проверяем наличие пары
-        assert self.APP.tab_schedule.check_lesson_on_page('Построение воспитывающей образовательной среды')
+        # Выбираем группу
+        self.APP.tab_schedule.select_group_or_teacher('47 С')
+        # Проверяем есть ли группа
+        assert self.APP.tab_schedule.check_group_or_teacher_on_page('47 С')
